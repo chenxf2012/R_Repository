6bc244b99c023e55758cee3bc4f40c528800390f
chenxf_2012 <chenxf_2012@163.com>
2019-07-20 00:48:54 +0800

    Test

---
+++ R_project/GitTest2.java
@@ -0,0 +1,11 @@
+package com.chenxf.web.service;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public interface GitTest {
+
+	public static void main(String[] args){
+		System.out.println("---test---");
+	}
+}
