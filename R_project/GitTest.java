e25eee7baf0476f46ecb0075a471ec37d0e49c0b
chenxf_2012 <chenxf_2012@163.com>
2019-07-31 11:55:02 +0800

    test3

--- R_project/GitTest.java
+++ R_project/GitTest.java
@@ -1,9 +1,6 @@
 package com.chenxf.web.service;
 
-import java.sql.SQLException;
-import java.util.Map;
-
-public interface GitTest {
+public class GitTest {
 
 	public static void main(String[] args){
 		System.out.println("---test---");
